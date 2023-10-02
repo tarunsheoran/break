be00cae86dca08495412d2ba0df290c1990deaac
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-02 22:28:58 +0530

    Add files via upload

---
+++ break.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 1; i <= 15; i++) {
+        if (i == 8) {
+            break;
+        }
+        cout << i << endl;
+    }
+return 0;
+}
